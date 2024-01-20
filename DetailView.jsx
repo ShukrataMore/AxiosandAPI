5e9ef12257fc443405344b86f18d92f1ec51334c
ShukrataMore <147790416+ShukrataMore@users.noreply.github.com>
2024-01-20 12:59:58 +0530

    Add files via upload

---
+++ DetailView.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+function DetailView() {
+    const { id  } = useParams();
+    const [rowData, setRowData] = useState(null);
+    
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/comments/${id}`)
+            .then((res) => setRowData(res.data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            <h2>Details for ID: {id}</h2>
+            {rowData ? (
+                <div>
+                    <ul>
+                        {Object.entries(rowData).map(([key, value]) => (
+                            <li key={key}>
+                                <strong>{key}:</strong> {value}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+}
+
+export default DetailView;
